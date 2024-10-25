c571ee8cba4f6d04706329403725a55e6ebabe78
Samuelcdev <samuuseche01@gmail.com>
2024-10-25 11:19:56 -0500

    Agregados 25102024

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,8 +1,13 @@
 export default function Carousel() {
     return (
-        <div className="carrousel">
-            <h1>Gestiona tus invernaderos con nosotros</h1>
-            <button>Empezar Ahora</button>
+        <div className="carousel">
+            <div className="container__image">
+                <img src="../public/images/invernadero.jpg" alt="" />
+            </div>
+            <div className="container__text">
+                <h1>Gestiona tus invernaderos con nosotros</h1>
+                <button>Empezar Ahora</button>
+            </div>
         </div>
     );
 }
---
+++ src/components/TeamCard.jsx
@@ -0,0 +1,16 @@
+export default function Team(){
+    return (
+        <div className="team">
+            <h1>Grupo de Investigacion</h1>
+            <article className="container__team">
+                <div className="image__team">
+                    <img src="https://img.freepik.com/foto-gratis/cierre-dientes-hombre-moreno-sonriendo_1187-5800.jpg" alt="" />
+                </div>
+                <div className="text__team">
+                    <h1>Samuel Useche Chaparro</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint porro recusandae, sit, consequuntur nulla ex sequi distinctio deleniti modi, id magni? Amet eum sint dolorem consequuntur cum magni nemo minus!</p>
+                </div>
+            </article>
+        </div>
+    );
+}
