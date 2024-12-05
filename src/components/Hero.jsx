9763e5547ff9cfca4160f76081c019b2bf704b0c
Samuelcdev <samuuseche01@gmail.com>
2024-12-05 11:11:35 -0500

    Agregados 5122024

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,39 @@
 const Hero = () => {
     return (
-      <section className="hero">
-        <div className="hero-container">
-          {/* Contenido de texto */}
-          <div className="hero-text">
-            <h1 className="hero-title">Estado del Arte del SENA</h1>
-            <p className="hero-description">
-            Presenta el estado del arte, es decir, una descripción del estado actual de la investigación sobre las competencias básicas. Nos permite conocer aspectos tales como la delimitación de la temática, los enfoques teóricos y metodológicos del entorno laboral y un acercamiento al concepto de competencia laboral. La investigación se centra en el estudio de las competencias consideradas básicas para el proceso de Formación Profesional Integral.
-            </p>
-          </div>
-        </div>
-      </section>
+        <section className="hero">
+            <div className="hero-container">
+                <div className="hero-text">
+                    <h1 className="hero-title">Estado del Arte del SENA</h1>
+                    <p className="hero-description">
+                        Presenta el estado del arte, es decir, una descripción
+                        del estado actual de la investigación sobre las
+                        competencias básicas. Nos permite conocer aspectos tales
+                        como la delimitación de la temática, los enfoques
+                        teóricos y metodológicos del entorno laboral y un
+                        acercamiento al concepto de competencia laboral. La
+                        investigación se centra en el estudio de las
+                        competencias consideradas básicas para el proceso de
+                        Formación Profesional Integral.
+                    </p>
+                </div>
+            </div>
+            <div className="hero__description">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Harum, doloremque perspiciatis distinctio natus provident
+                    accusamus! Eligendi, accusamus! Est voluptate, nisi id
+                    aliquam eligendi odio veniam eum sit, repudiandae tempora
+                    amet!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quos, fugiat. Sed delectus esse, id obcaecati dolores
+                    eveniet ducimus expedita nostrum eius neque fugit cumque
+                    culpa quibusdam, tempora optio repudiandae blanditiis.
+                </p>
+            </div>
+        </section>
     );
-  };
-  
-  export default Hero;
+};
+
+export default Hero;
--- src/components/data/CardInvernaderoList.js
+++ src/components/data/CardInvernaderoList.js
@@ -7,27 +7,27 @@ const cardsList = [
     {
         id: 2,
         title: "Actualización",
-        image: "https://cdn-icons-png.flaticon.com/512/5278/5278646.png",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXgSsW2-pPC8-PoUcUZG6LtCuG8kmIBDXfLg&s",
     },
     {
         id: 3,
         title: "Actualización",
-        image: "https://cdn-icons-png.flaticon.com/512/5278/5278646.png",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXgSsW2-pPC8-PoUcUZG6LtCuG8kmIBDXfLg&s",
     },
     {
         id: 3,
         title: "Actualización",
-        image: "https://cdn-icons-png.flaticon.com/512/5278/5278646.png",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXgSsW2-pPC8-PoUcUZG6LtCuG8kmIBDXfLg&s",
     },
     {
         id: 3,
         title: "Actualización",
-        image: "https://cdn-icons-png.flaticon.com/512/5278/5278646.png",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXgSsW2-pPC8-PoUcUZG6LtCuG8kmIBDXfLg&s",
     },
     {
         id: 3,
         title: "Actualización",
-        image: "https://cdn-icons-png.flaticon.com/512/5278/5278646.png",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXgSsW2-pPC8-PoUcUZG6LtCuG8kmIBDXfLg&s",
     },
 ];
 
